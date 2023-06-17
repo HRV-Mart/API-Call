0f98454fe96affb645ef214cb1faa4de7968f22e
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-06-17 14:27:57 +0530

    Add response in all request (#26)
    * Added response
    
    * Added default message

--- src/main/kotlin/com/hrv/mart/apicall/APICaller.kt
+++ src/main/kotlin/com/hrv/mart/apicall/APICaller.kt
@@ -3,9 +3,12 @@ package com.hrv.mart.apicall
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
+import org.springframework.http.server.reactive.ServerHttpResponse
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient.Builder
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import reactor.core.publisher.Mono
+import kotlin.reflect.full.findAnnotation
 
 @Service
 class APICaller(
@@ -13,14 +16,26 @@ class APICaller(
     private val webClientBuilder: Builder
 )
 {
-    fun <T>getData(path: String, classType: Class<T>): Mono<T> {
+    fun <T>getData(
+        path: String,
+        responseClassType: Class<T>,
+        response: ServerHttpResponse
+    ): Mono<T> {
         val webClient = webClientBuilder.baseUrl(path)
             .build()
         return webClient.get()
             .retrieve()
-            .bodyToMono(classType)
+            .bodyToMono(responseClassType)
+            .onErrorResume {
+                setResponse(response, it as WebClientResponseException, responseClassType)
+            }
     }
-    fun <T, U : Any>postRequest(path: String, responseClassType: Class<T>, requestBody: U): Mono<T> {
+    fun <T, U : Any>postRequest(
+        path: String,
+        responseClassType: Class<T>,
+        requestBody: U,
+        response: ServerHttpResponse
+    ): Mono<T> {
         val webClient = webClientBuilder.baseUrl(path)
             .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
             .build()
@@ -29,8 +44,16 @@ class APICaller(
             .body(Mono.just(requestBody), requestBody::class.java)
             .retrieve()
             .bodyToMono(responseClassType)
+            .onErrorResume {
+                setResponse(response, it as WebClientResponseException, responseClassType)
+            }
     }
-    fun <T, U : Any>putRequest(path: String, responseClassType: Class<T>, requestBody: U): Mono<T> {
+    fun <T, U : Any>putRequest(
+        path: String,
+        responseClassType: Class<T>,
+        requestBody: U,
+        response: ServerHttpResponse
+    ): Mono<T> {
         val webClient = webClientBuilder.baseUrl(path)
             .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
             .build()
@@ -39,12 +62,31 @@ class APICaller(
             .body(Mono.just(requestBody), requestBody::class.java)
             .retrieve()
             .bodyToMono(responseClassType)
+            .onErrorResume {
+                setResponse(response, it as WebClientResponseException, responseClassType)
+            }
     }
-    fun <T>deleteData(path: String, classType: Class<T>): Mono<T> {
+    fun <T>deleteData(
+        path: String,
+        responseClassType: Class<T>,
+        response: ServerHttpResponse
+    ): Mono<T> {
         val webClient = webClientBuilder.baseUrl(path)
             .build()
         return webClient.delete()
             .retrieve()
-            .bodyToMono(classType)
+            .bodyToMono(responseClassType)
+            .onErrorResume {
+                setResponse(response, it as WebClientResponseException, responseClassType)
+            }
+    }
+    private fun <T >setResponse(response: ServerHttpResponse, error: WebClientResponseException, responseClassType: Class<T>,): Mono<T> {
+        response.statusCode = error.statusCode
+        return if (responseClassType::class.java == String::class.java) {
+            val message = (error.message ?: "") as T
+            Mono.just(message)
+        } else {
+            Mono.empty()
+        }
     }
 }
